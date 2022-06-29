04193296a5d61d5b1855c3628d6fc639897fb7b9
Seungbaek Lee <yahoo557@naver.com>
2022-06-30 00:14:02 +0900

    클라이언트 추가

---
+++ server/config/db.config.js
@@ -0,0 +1,3 @@
+const client = require('mongodb')
+
+client.connect()
---
+++ server/config/ws.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    server: https // WebSocket서버에 연결할 HTTP서버를 지정한다.
+    // port: 30002 // WebSocket연결에 사용할 port를 지정한다(생략시, http서버와 동일한 port 공유 사용)
+}
