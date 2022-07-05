21d5f382fa164a1e1930467e133dba4500f6188f
Seungbaek Lee <yahoo557@naver.com>
2022-07-05 11:04:27 +0900

    db.config.js 수정 : mongoDB 연동

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,3 +1,21 @@
-const client = require('mongodb')
+const { MongoClient } = require('mongodb');
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
 
-client.connect()
+const dbName = 'chat'
+async function main() {
+    // Use connect method to connect to the server
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db(dbName);
+    const collection = db.collection('documents');
+  
+    // the following code examples can be pasted here...
+  
+    return 'done.';
+  }
+  
+  main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());  
