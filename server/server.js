805b25f3d2d2852bd8a8b58a7b9ae329127b785c
Seungbaek Lee <yahoo557@naver.com>
2022-07-06 17:57:03 +0900

    router 관리

--- server/server.js
+++ server/server.js
@@ -6,6 +6,9 @@ const https_option = require("./config/ssl.config")
 const ws = require('ws')
 const port = 8000
 
+
+
+app.engine('html', require('ejs').renderFile);
 app.get("/", (req,res)=>{
     const nDate = new Date().toLocaleString('kr-KR', {
         timeZone: 'Asia/Seoul'
@@ -13,21 +16,8 @@ app.get("/", (req,res)=>{
     return res.status(200).send(nDate)
 })
 
-const https_server = https.createServer(https_option, app).listen(port, function () {
-    console.log("HTTPS server listening on port " + port);
-});
-
-
-
-const ws_server = new ws.Server({server : https_server});
-
-ws_server.on('connection', (ws, req)=>{
-    //연결 클라이언트의 IP 획득
-    const ip = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
-    console.log(`새로운 클라이언트[${ip}] 접속`)
-    if(ws.readyState === ws.OPEN){ // 연결 여부 체크
-        ws.send(`클라이언트[${ip}] 접속을 환영합니다 from 서버`); // 데이터 전송
-    }
-
-
+const routers = [
+  ['/char', './router/chat.js']]
+routers.forEach(k => {
+  app.use(k[0], require(k[1]));
 })
