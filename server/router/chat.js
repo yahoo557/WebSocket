083b462cb4f2a6edf346f13c969cc723f3178b7d
Seungbaek Lee <yahoo557@naver.com>
2022-07-08 00:43:37 +0900

    성공, 근데 어케 했지;;

--- server/router/chat.js
+++ server/router/chat.js
@@ -1,63 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const wsModule = require('ws');
-// const HTTPSServer = require("../config/https.config")
 
-router.get("/:id", (req, res) => {
-    const room_id = req.params.id
-
+router.get("/", (req, res) => {
 
     return res.status(200).render("../public/chat.ejs")
 })
-
-
-
-// 2. WebSocket 서버 생성/구동
-const webSocketServer = new wsModule.Server(
-    {
-        // server: HTTPSServer, // WebSocket서버에 연결할 HTTP서버를 지정한다.
-        port: 8001 // WebSocket연결에 사용할 port를 지정한다(생략시, http서버와 동일한 port 공유 사용)
-    }
-);
-
-
-// connection(클라이언트 연결) 이벤트 처리
-webSocketServer.on('connection', (ws, request) => {
-    // 1) 연결 클라이언트 IP 취득
-    const ip = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
-    console.log(`새로운 클라이언트[${ip}] 접속`);
-
-    // 2) 클라이언트에게 메시지 전송
-    if (ws.readyState === ws.OPEN) { // 연결 여부 체크
-        ws.send(`클라이언트[${ip}] 접속을 환영합니다 from 서버`); // 데이터 전송
-    }
-
-    // 3) 클라이언트로부터 메시지 수신 이벤트 처리
-    ws.on('message', (msg) => {
-        function broadcast(msg) {       // (4)
-            for (const client of ws.clients) {
-                if (client.readyState === ws.OPEN) {
-                    client.send(msg)
-                }
-            }
-        }
-        
-        console.log(`클라이언트[${ip}]에게 수신한 메시지 : ${msg}`);
-        broadcast(msg);
-    })
-
-    // 4) 에러 처러
-    ws.on('error', (error) => {
-        console.log(`클라이언트[${ip}] 연결 에러발생 : ${error}`);
-    })
-
-    // 5) 연결 종료 이벤트 처리
-    ws.on('close', () => {
-        console.log(`클라이언트[${ip}] 웹소켓 연결 종료`);
-    })
-});
-
-
 
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,29 +1,44 @@
 const express = require("express")
 const app = express();
+const wsModule = require('ws');
 const chat = require("./router/chat")
 app.use(express.json());
 app.use("/chat", chat)
 const https = require("https")
 const https_option = require("./config/ssl.config")
 const ws = require('ws')
+const { WebSocketServer } = require('ws');
 const port = 8000
+app.engine('html', require('ejs').renderFile);
 
-const HTTPSServer = https.createServer(https_option, app).listen(port, function () {
+app.get("/", (req, res) => {
+  const nDate = new Date().toLocaleString('kr-KR', {
+    timeZone: 'Asia/Seoul'
+  });
+  return res.status(200).send(nDate)
+})
+
+const server = https.createServer(https_option, app).listen(port, function () {
   console.log("HTTPS server listening on port " + port);
 });
 
-module.exports = HTTPSServer
-app.engine('html', require('ejs').renderFile);
 
-app.get("/", (req,res)=>{
-    const nDate = new Date().toLocaleString('kr-KR', {
-        timeZone: 'Asia/Seoul'
-      });
-    return res.status(200).send(nDate)
+
+
+//serve static folder
+
+const wss = new WebSocketServer({  port:8001 }) // (2)
+wss.on('connection', (client) => {
+  console.log('Client connected !')
+  client.on('message', (msg) => {    // (3)
+    console.log(`Message:${msg}`);
+    broadcast(msg)
+  })
 })
-
-// const routers = [
-//   ['/chat', './router/chat.js']]
-// routers.forEach(k => {
-//   app.use(k[0], require(k[1]));
-// })
+function broadcast(msg) {       // (4)
+  for (const client of wss.clients) {
+    if (client.readyState === ws.OPEN) {
+      client.send(msg)
+    }
+  }
+}
