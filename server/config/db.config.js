f2252c00bfe4b60f6922689240cb01c00211acfc
Seungbaek Lee <yahoo557@naver.com>
2022-07-08 02:51:13 +0900

    몽고 db 연동

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -3,19 +3,19 @@ const url = 'mongodb://localhost:27017';
 const client = new MongoClient(url);
 
 const dbName = 'chat'
+
+
 async function main() {
-    // Use connect method to connect to the server
-    await client.connect();
-    console.log('Connected successfully to server');
-    const db = client.db(dbName);
-    const collection = db.collection('documents');
-  
-    // the following code examples can be pasted here...
-  
-    return 'done.';
-  }
-  
-  main()
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const collection = db.collection('documents');
+  return 'done.';
+}
+
+main()
   .then(console.log)
   .catch(console.error)
-  .finally(() => client.close());  
+  .finally(() => client.close());
+
+module.exports = client
--- server/router/chat.js
+++ server/router/chat.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const client = require("../config/db.config")
 
 router.get("/", (req, res) => {
 
